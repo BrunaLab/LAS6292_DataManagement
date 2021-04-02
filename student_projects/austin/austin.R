0bcf8fd76c250eda8bb24d053e26696aa7da05c8
Emilio M. Bruna <embruna@Emilios-MacBook-Air.local>
2021-04-02 16:13:25 -0400

    12 class corrections

--- student_projects/austin/austin.R
+++ student_projects/austin/austin.R
@@ -1,7 +1,7 @@
 # pdf of labels to text
 # library(magick) #convert jpeg to pdf
 # library(pdftools) # extract text from pdf
-library(tesseract) #do all in one
+
 # # read the jpeg
 # label_1 <- image_read("./IMG_0074.JPG")
 # image_info(label_1)
@@ -10,17 +10,15 @@ library(tesseract) #do all in one
 # image_info(label_1_pdf)
 # # save it to the folder
 # image_write(label_1_pdf, "./label_1.pdf")
-# 
-# 
-# pdf_file <- "./test.pdf"
+# # # pdf_file <- "./test.pdf"
 # pdf_file <- "./IMG_0074_export.pdf"
 # txt <- pdf_text(pdf_file)
 # cat(txt[1])
 # 
-# 
+library(tesseract) #do all in one
 tesseract_info() # what languages do you have installed?
 tesseract_download("por") # install portuguese
-text <- tesseract::ocr("./austin/IMG_0074.JPG", engine = "por")
+text <- tesseract::ocr("./student_projects/austin/IMG_0074.JPG", engine = "por")
 cat(text)
 # you can see requires some cleanup 
 # (note the \n and \a etc indicate line breaks, etc) but not much
