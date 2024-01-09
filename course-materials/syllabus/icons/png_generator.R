1fae1a2c7c720c22573dbc1ce47fd2f61ed885dd
embruna <embruna@ufl.edu>
2024-01-08 22:06:04 -0500

    update syllabus for 2024

---
+++ course-materials/syllabus/icons/png_generator.R
@@ -0,0 +1,43 @@
+library(fontawesome)
+# find the icon on https://fontawesome.com/icons, then save to computer 
+# using the name of the icon. no need to download svg and convert
+
+
+
+
+if (interactive()) {
+  # Create a Font Awesome SVG icon as a
+  # PNG file on disk
+  
+  fa_png(name = "folder-tree",
+         file="./icons/folder-tree.png",
+         fill="black")
+  
+}
+
+
+# thumbs-up
+# headphones
+# scale-balanced
+# person-breastfeeding
+# microphone
+# computer
+# check
+# person-chalkboard
+# forestgreen
+# envelope-circle-check
+# person-running
+# mug-saucer
+# bullhorn
+# house-user
+# person-chalkboard
+# volume-xmark
+# calendar-check
+# calendar-xmark
+# calendar-plus
+# bolt
+# image
+# folder-tree
+# microphone
+# pencil
+# magnifying-glass-chart
